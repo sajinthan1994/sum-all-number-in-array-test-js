54de4ed27a295223f411a53f2365cc7edcf909db
Sayinthan Theivasothy <t.saj1994@gmail.com>
2020-03-06 23:30:47 +0000

    Sums numbers from nested arrays

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+  array.forEach(element => {
+    if (!Array.isArray(element)) {
+      sum += element;
+    } else {
+      sum += sumItems(element);
+    }
+  });
+  return sum;
 }
 
+console.log(sumItems([1,[2,[3,6,8,[1,2,3,[6,9]]],4,5]]));
 module.exports = sumItems;
